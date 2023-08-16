2020db8c9e7ff54a779e4d58522639c6cf29bcdf
Nahom1927 <yheys898184@gmail.com>
2023-08-16 23:43:01 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-
-// Makes API request, sets async to allow await promise
-request(filmURL + filmNum, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  // find URLs of each character in the film as a list obj
-  const charURLList = JSON.parse(body).characters;
-
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(filmUrl, (error, response, body) => {
+  if (error) {
+    return;
   }
+  const film = JSON.parse(body);
+  const urlToName = {};
+  film.characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        return;
+      }
+      const character = JSON.parse(body);
+      urlToName[character.url] = character.name;
+      if (Object.keys(urlToName).length === film.characters.length) {
+        film.characters.forEach(characterUrl =>
+          console.log(urlToName[characterUrl]));
+      }
+    });
+  });
 });
