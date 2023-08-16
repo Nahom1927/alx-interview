12baca3d6cfaf4c1048877331383a99c0f1c397d
Nahom1927 <yheys898184@gmail.com>
2023-08-16 23:36:15 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,63 +1,39 @@
 #!/usr/bin/python3
+""" program that solves the N queens problem """
 import sys
 
-possibilties = []
-for line in sys.stdin:
-    try:
-        possibilties.append(eval(line))
-    except Exception as e:
-        pass
 
-def check_two_queens(new_queen_position, other_queen_position):
-    if new_queen_position[0] == other_queen_position[0] or new_queen_position[1] == other_queen_position[1]:
-        return False
-    if new_queen_position[0] + new_queen_position[1] == other_queen_position[0] + other_queen_position[1]:
-        return False
-    if new_queen_position[0] - new_queen_position[1] == other_queen_position[0] - other_queen_position[1]:
-        return False
+def isSafe(Board, line, i):
+    """ checks if we can insert queen at column i in that line in Board"""
+    for x in range(line):
+        if (Board[x] == i or
+                Board[x] + line - x == i or
+                Board[x] + x - line == i):
+            return False
     return True
 
 
-def possibilty_already_done(possibilty, possibilties_done):
-    if len(possibilties_done) == 0:
-        return False
-    for possibilty_done in possibilties_done:
-        nb_similar = 0
-        for position in possibilty:
-            nb_similar += 1 if position in possibilty_done else 0
-        if nb_similar == len(possibilty_done):
-            return True
-    return False
+def Fill_line(Board, line):
+    """ fills each line of the board with the correct index """
+    for i in range(len(Board)):
+        if isSafe(Board, line, i):
+            Board[line] = i
+            if line < len(Board) - 1:
+                Fill_line(Board, line + 1)
+            else:
+                print([[i, Board[i]] for i in range(len(Board))])
 
-result = True
-possibilties_done = []
-for possibilty in possibilties:
-    i = 0
-    while i < len(possibilty) -1:
-        j = i + 1
-        while j < len(possibilty):
-            result = check_two_queens(possibilty[i], possibilty[j])
-            if not result:
-                break
-            j += 1
-        i += 1
-        if not result:
-            break
-    if not result:
-        break
-    else:
-        # good possibilty
-        if possibilty_already_done(possibilty, possibilties_done):
-            result = False
-        else:
-            possibilties_done.append(possibilty)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    if not result:
-        break
-    
-
-if result:
-    print("OK")
-else:
-    print("NOK")
-print(len(possibilties))
+Board = [-1 for i in range(n)]
+Fill_line(Board, 0)
