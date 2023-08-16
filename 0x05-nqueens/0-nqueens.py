184ce0c11c466124542f690dfd36b15cd5bb4d0d
Nahom1927 <yheys898184@gmail.com>
2023-08-16 23:37:57 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,39 +1,56 @@
 #!/usr/bin/python3
-""" program that solves the N queens problem """
-import sys
+
+'''
+N queens
+'''
+
+from sys import argv
 
 
-def isSafe(Board, line, i):
-    """ checks if we can insert queen at column i in that line in Board"""
-    for x in range(line):
-        if (Board[x] == i or
-                Board[x] + line - x == i or
-                Board[x] + x - line == i):
+def is_NQueen(cell: list) -> bool:
+    '''
+    True if N Queen, False if not
+    '''
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
             return False
     return True
 
 
-def Fill_line(Board, line):
-    """ fills each line of the board with the correct index """
-    for i in range(len(Board)):
-        if isSafe(Board, line, i):
-            Board[line] = i
-            if line < len(Board) - 1:
-                Fill_line(Board, line + 1)
-            else:
-                print([[i, Board[i]] for i in range(len(Board))])
+def solve_NQeens(dimension: int, row: int, cell: list, outcome: list):
+    """
+    Return result of N Queens recusrivley
+    """
+    # Base case
+    if row == dimension:
+        print(outcome)
+    else:
+        for col in range(0, dimension):
+            cell.append(col)
+            outcome.append([row, col])
+            if (is_NQueen(cell)):
+                solve_NQeens(dimension, row + 1, cell, outcome)
+            cell.pop()
+            outcome.pop()
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
 try:
-    n = int(sys.argv[1])
-except:
-    print("N must be a number")
-    sys.exit(1)
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-Board = [-1 for i in range(n)]
-Fill_line(Board, 0)
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    outcome = []
+    cell = 0
+    solve_NQeens(int(N), cell, [], outcome)
