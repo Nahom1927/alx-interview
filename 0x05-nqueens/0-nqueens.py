29da112e8b5ce9ee811febf3dfb6fe4e98f89c5e
Nahom1927 <yheys898184@gmail.com>
2023-10-20 21:41:50 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,63 +1,59 @@
 #!/usr/bin/python3
-def n_queens(n):
-  """Solve the n-queens problem.
 
-  Args:
-    n: The number of queens.
+#!/usr/bin/python3
 
-  Returns:
-    A list of lists, where each inner list represents a row of the chessboard, and
-    each queen is represented by a 'Q'.
-  """
-  if n <= 0:
-    return []
+import sys
 
-  # Initialize the board to all empty squares.
-  board = [['.' for _ in range(n)] for _ in range(n)]
 
-  # Recursively place the queens on the board.
-  def place_queens(row, queens_placed):
-    if row == n:
-      return True
+def solve(row, column):
+    solver = [[]]
+    for q in range(row):
+        solver = place_queen(q, column, solver)
+    return solver
 
-    for col in range(n):
-      if is_valid(board, row, col):
-        board[row][col] = 'Q'
-        if place_queens(row + 1, queens_placed + 1):
-          return True
 
-        board[row][col] = '.'
+def place_queen(q, column, prev_solver):
+    solver_queen = []
+    for array in prev_solver:
+        for x in range(column):
+            if is_safe(q, x, array):
+                solver_queen.append(array + [x])
+    return solver_queen
 
-    return False
 
-  return place_queens(0, 0)
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
-def is_valid(board, row, col):
-  """Check if the given position is valid for a queen.
 
-  Args:
-    board: The board.
-    row: The row of the queen.
-    col: The column of the queen.
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        the_queen = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if the_queen < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return(the_queen)
 
-  Returns:
-    True if the position is valid, False otherwise.
-  """
-  # Check if the queen is in the same row as another queen.
-  for i in range(row):
-    if board[i][col] == 'Q':
-      return False
 
-  # Check if the queen is in the same column as another queen.
-  for i in range(n):
-    if board[row][i] == 'Q':
-      return False
+def n_queens():
 
-  # Check if the queen is in the same diagonal as another queen.
-  for i in range(-row, row + 1):
-    for j in range(-col, col + 1):
-      if i != 0 and j != 0:
-        if board[row + i][col + j] == 'Q':
-          return False
+    the_queen = init()
+    solver = solve(the_queen, the_queen)
+    for array in solver:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
 
-  return True
+
+if __name__ == '__main__':
+    n_queens()
