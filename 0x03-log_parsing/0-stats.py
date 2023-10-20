27b6a3ba0858a8d3ec5f781a1ad4752f9c8ff8e8
Nahom1927 <yheys898184@gmail.com>
2023-10-20 18:03:59 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,41 @@
 #!/usr/bin/python3
-import random
+"""Log Parsing
+Write a script that reads stdin line by line and computes metrics:
+"""
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
 
-alexa@ubuntu:~/0x03-log_parsing$ ./0-generator.py | ./0-stats.py 
-File size: 5213
-200: 2
-401: 1
-403: 2
-404: 1
-405: 1
-500: 3
-File size: 11320
-200: 3
-301: 2
-400: 1
-401: 2
-403: 3
-404: 4
-405: 2
-500: 3
-File size: 16305
-200: 3
-301: 3
-400: 4
-401: 2
-403: 5
-404: 5
-405: 4
-500: 4
-^CFile size: 17146
-200: 4
-301: 3
-400: 4
-401: 2
-403: 6
-404: 6
-405: 4
-500: 4
-Traceback (most recent call last):
-  File "./0-stats.py", line 15, in <module>
-Traceback (most recent call last):
-  File "./0-generator.py", line 8, in <module>
-    for line in sys.stdin:
-KeyboardInterrupt
-    sleep(random.random())
+total_file_size = 0
+status = ['200', '301', '400', '401', '403', '404', '405', '500']
+obj = dict.fromkeys(status, 0)
+
+
+def printLogStat():
+    """Print log statistics"""
+    print("File size: {}".format(total_file_size))
+    for key, value in sorted(obj.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    count = 0
+    try:
+        for line in sys.stdin:
+            line = line.split()
+            count += 1
+            try:
+                total_file_size += int(line[-1])
+
+                if line[-2] in status:
+                    obj[line[-2]] += 1
+
+            except (IndexError, ValueError):
+                pass
+
+            if count % 10 == 0:
+                printLogStat()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        printLogStat()
