fdd283c271d55e0670f6fb6fb2dc9c2d440a4281
Nahom1927 <yheys898184@gmail.com>
2023-10-20 21:36:50 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,40 +1,63 @@
 #!/usr/bin/python3
-def nqueens(n):
-  """
-  Finds all possible ways to place n queens on an n×n chessboard such that no two queens attack each other.
+def n_queens(n):
+  """Solve the n-queens problem.
 
   Args:
     n: The number of queens.
 
   Returns:
-    A list of all possible placements of the queens.
+    A list of lists, where each inner list represents a row of the chessboard, and
+    each queen is represented by a 'Q'.
   """
-  solutions = []
-  def backtrack(row, queens):
-    if row == n:
-      solutions.append(queens)
-      return
-    for i in range(n):
-      if is_valid(row, i, queens):
-        queens.append((row, i))
-        backtrack(row + 1, queens)
-        queens.pop()
-  backtrack(0, [])
-  return solutions
+  if n <= 0:
+    return []
 
-def is_valid(row, col, queens):
-  """
-  Checks if a queen can be placed at the given row and column.
+  # Initialize the board to all empty squares.
+  board = [['.' for _ in range(n)] for _ in range(n)]
+
+  # Recursively place the queens on the board.
+  def place_queens(row, queens_placed):
+    if row == n:
+      return True
+
+    for col in range(n):
+      if is_valid(board, row, col):
+        board[row][col] = 'Q'
+        if place_queens(row + 1, queens_placed + 1):
+          return True
+
+        board[row][col] = '.'
+
+    return False
+
+  return place_queens(0, 0)
+
+def is_valid(board, row, col):
+  """Check if the given position is valid for a queen.
 
   Args:
+    board: The board.
     row: The row of the queen.
     col: The column of the queen.
-    queens: A list of all the queens that have already been placed.
 
   Returns:
-    True if the queen can be placed at the given row and column, False otherwise.
+    True if the position is valid, False otherwise.
   """
-  for queen in queens:
-    if queen[0] == row or queen[1] == col or abs(queen[0] - row) == abs(queen[1] - col):
+  # Check if the queen is in the same row as another queen.
+  for i in range(row):
+    if board[i][col] == 'Q':
       return False
+
+  # Check if the queen is in the same column as another queen.
+  for i in range(n):
+    if board[row][i] == 'Q':
+      return False
+
+  # Check if the queen is in the same diagonal as another queen.
+  for i in range(-row, row + 1):
+    for j in range(-col, col + 1):
+      if i != 0 and j != 0:
+        if board[row + i][col + j] == 'Q':
+          return False
+
   return True
