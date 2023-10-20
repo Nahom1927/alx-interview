142e9d5f36a63f9675a314133b5873f978f63046
Nahom1927 <yheys898184@gmail.com>
2023-10-20 17:37:11 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,48 @@
 #!/usr/bin/python3
-import random
+"""
+Task - Script that reads stdin line by line and computes metrics
+"""
+
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
 
-alexa@ubuntu:~/0x03-log_parsing$ ./0-generator.py | ./0-stats.py 
-File size: 5213
-200: 2
-401: 1
-403: 2
-404: 1
-405: 1
-500: 3
-File size: 11320
-200: 3
-301: 2
-400: 1
-401: 2
-403: 3
-404: 4
-405: 2
-500: 3
-File size: 16305
-200: 3
-301: 3
-400: 4
-401: 2
-403: 5
-404: 5
-405: 4
-500: 4
-^CFile size: 17146
-200: 4
-301: 3
-400: 4
-401: 2
-403: 6
-404: 6
-405: 4
-500: 4
-Traceback (most recent call last):
-  File "./0-stats.py", line 15, in <module>
-Traceback (most recent call last):
-  File "./0-generator.py", line 8, in <module>
-    for line in sys.stdin:
-KeyboardInterrupt
-    sleep(random.random())
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 1
+    file_size = 0
+
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
+
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
